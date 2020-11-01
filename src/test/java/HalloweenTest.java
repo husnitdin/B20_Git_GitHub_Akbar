1f71de0a2ba8c108ed0af4c0973d1a3dc179875e
husnitdin <husnitdin@gmail.com>
2020-11-01 22:23:23 +0500

    initial commit

--- src/test/java/HalloweenTest.java
+++ src/test/java/HalloweenTest.java
@@ -1,6 +1,7 @@
 public class HalloweenTest {
     public static void main(String[] args) {
         System.out.println("hello halloween test");
+        System.out.println("second change");
     }
 }
 
