ca992466bdcf698b44b63d879bc2dc7cabc6c2a7
husnitdin <husnitdin@gmail.com>
2020-11-01 22:35:04 +0500

    third commit

--- src/test/java/HalloweenTest.java
+++ src/test/java/HalloweenTest.java
@@ -2,6 +2,7 @@ public class HalloweenTest {
     public static void main(String[] args) {
         System.out.println("hello halloween test");
         System.out.println("second change");
+        System.out.println("third change");
     }
 }
 
